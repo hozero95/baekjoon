db7e88093260c32df5c1b8d6203a6aaf96e9aa39
hozero95 <the.hozero@gmail.com>
2021-08-26 21:15:49 +0900

    2021-08-26

--- src/com/company/no_17298/Main.java
+++ src/com/company/no_17298/Main.java
@@ -4,26 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 모르겠다 ㅎㅎ
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String[] str = br.readLine().split(" ");
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(str[i]);
         }
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             while (!stack.empty() && arr[stack.peek()] < arr[i]) {
                 arr[stack.pop()] = arr[i];
             }
-
             stack.push(i);
         }
 
@@ -32,9 +30,8 @@ public class Main {
         }
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            sb.append(arr[i] + " ");
         }
-
-        br.close();
+        System.out.println(sb);
     }
 }
