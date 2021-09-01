fe9dd8c027f60cbde66e790f15eaf5d87ae41270
hozero95 <the.hozero@gmail.com>
2021-09-01 22:40:00 +0900

    2021-09-01

--- src/com/company/no_1406/Main.java
+++ src/com/company/no_1406/Main.java
@@ -2,7 +2,7 @@ package com.company.no_1406;
 
 import java.io.*;
 import java.util.Stack;
-
+//
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
